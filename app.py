dc6fa13a1cb4848c1a539aef8367afa64264a129
Marina Saitgareeva <4006761ms@gmail.com>
2022-11-20 13:40:23 -0500

    fixed error in artist search route

--- app.py
+++ app.py
@@ -492,14 +492,14 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
+  # DONE: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
 
   # Create a variable to store user input data from venue's search form.
   search_term = request.form.get('search_term', '')
   # Create variable to store search result from the database (fyyur) based on the user input data from artist's search form.
-  search_result = Venue.query.filter(Artist.name.ilike('%' + search_term + '%')).all()
+  search_result = Artist.query.filter(Artist.name.ilike('%' + search_term + '%')).all()
   # Create a list variable to store artists from search result.
   artist_list = []
   # Iterate over each artist in search result and add then in artist list.
